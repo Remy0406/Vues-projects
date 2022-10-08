b98699064b644aa30d654d82d22f92c5db279a96
Remy0406 <112329579+Remy0406@users.noreply.github.com>
2022-10-08 12:50:08 +0700

    Add files via upload

---
+++ src/pages/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <navbar />
+  <section class="home">
+
+    <div class="image">
+        <img src="images/rm.jpg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>hi, i'm Panisara Hangwongngam</h3>
+        <span>Belive the Impossible is Possible.</span>
+        <p>Remy!</p>
+        <a href="about.html" class="btn"> about me <i class="fas fa-user"></i> </a>
+    </div>
+
+</section>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue'
+export default {
+  components: { Navbar },
+
+}
+</script>
+
+<style>
+
+</style>
